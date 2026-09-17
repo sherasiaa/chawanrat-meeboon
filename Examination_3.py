7292f22ffa268c3de450bd78977c298e74278cee
sherasiaa <qsaltry@gmail.com>
2026-09-17 09:49:56 +0700

    Update Examination_3.py

--- Examination_3.py
+++ Examination_3.py
@@ -7,7 +7,7 @@
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
 price = float(input("ราคาสินค้าเต็ม "))
-is_member = int(input("สถานะสมาชิก( 1 คือเป็นสมาชิก, 0 คือไม่เป็น) "))               
+is_member = int(input("สถานะสมาชิก( 1 คือเป็นสมาชิก, 0 คือไม่เป็น ) "))               
 discount = price *10/100
 net_price = price - discount
 if is_member == 1 :
