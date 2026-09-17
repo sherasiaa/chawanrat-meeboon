186cb6241e4cff5f8a58a95a9689d9032a63362c
sherasiaa <qsaltry@gmail.com>
2026-09-17 09:33:58 +0700

    Update Examination_5.py

--- Examination_5.py
+++ Examination_5.py
@@ -7,4 +7,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-bmi = float(input())
+bmi = float(input("ค่า BMI"))
+if bmi < 18.5 :
+    print("Underweight")
+elif bmi <= 22.9 :
+    print("Normal")
+else :
+    print("Overweight")
